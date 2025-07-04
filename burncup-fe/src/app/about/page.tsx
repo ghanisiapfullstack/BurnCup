91d7a80a6780feceae2cc282ae7926b8fc684f9c
Kareem <68719065+KareemHafidzh@users.noreply.github.com>
2025-07-04 17:53:56 +0700

    updated About Us Page

--- burncup-fe/src/app/about/page.tsx
+++ burncup-fe/src/app/about/page.tsx
@@ -7,155 +7,155 @@ export default function AboutPage() {
       <main className="p-10">
         <h1 className="text-4xl font-bold mb-4 text-center">The Burncup Journey</h1>
 
-        {/* First section: Desc left, Image right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-10">
-            <div className="flex-[2] max-w-md flex flex-col items-center">
-                <h1 className= " text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2025</h1>
-                <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
-                <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
-            </div>
-            <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
-                <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
-            </div>
+        {/* Section 1: Desc left, Image right */}
+        <div className="flex flex-row justify-center gap-50 mt-10">
+          <div className="flex-[2] max-w-md flex flex-col items-center">
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2025</h1>
+            <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
+            <p className="text-[26px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-6 w-fit">
+            <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
+          </div>
         </div>
 
-        {/* Second section: Image left, Desc right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-          <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
+        {/* Section 2: Image left, Desc right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="grid grid-cols-2 gap-6 w-fit">
             <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
           </div>
           <div className="flex-[2] max-w-md flex flex-col items-center">
-            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2024</h1>
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2024</h1>
             <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
             <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
           </div>
         </div>
 
-        {/* Third section: Desc left, Image right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-            <div className="flex-[2] max-w-md flex flex-col items-center">
-                <h1 className= " text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2023</h1>
-                <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
-                <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
-            </div>
-            <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
-                <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
-            </div>
+        {/* Section 3: Desc left, Image right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="flex-[2] max-w-md flex flex-col items-center">
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2023</h1>
+            <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
+            <p className="text-[26px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-6 w-fit">
+            <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
+          </div>
         </div>
 
-        {/* Fourth section: Image left, Desc right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-          <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
+        {/* Section 4: Image left, Desc right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="grid grid-cols-2 gap-6 w-fit">
             <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
           </div>
           <div className="flex-[2] max-w-md flex flex-col items-center">
-            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2022</h1>
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2022</h1>
             <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
             <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
           </div>
         </div>
 
-        {/* Fifth section: Desc left, Image right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-            <div className="flex-[2] max-w-md flex flex-col items-center">
-                <h1 className= " text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2021</h1>
-                <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
-                <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
-            </div>
-            <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
-                <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
-            </div>
+        {/* Section 5: Desc left, Image right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="flex-[2] max-w-md flex flex-col items-center">
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2021</h1>
+            <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
+            <p className="text-[26px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-6 w-fit">
+            <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
+          </div>
         </div>
 
-        {/* Sixth section: Image left, Desc right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-          <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
+        {/* Section 6: Image left, Desc right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="grid grid-cols-2 gap-6 w-fit">
             <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
           </div>
           <div className="flex-[2] max-w-md flex flex-col items-center">
-            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2020</h1>
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2020</h1>
             <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
             <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
           </div>
         </div>
 
-        {/* Seventh section: Desc left, Image right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-            <div className="flex-[2] max-w-md flex flex-col items-center">
-                <h1 className= " text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2019</h1>
-                <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
-                <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
-            </div>
-            <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
-                <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
-                <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
-            </div>
+        {/* Section 7: Desc left, Image right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="flex-[2] max-w-md flex flex-col items-center">
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2019</h1>
+            <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
+            <p className="text-[26px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-6 w-fit">
+            <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
+            <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
+          </div>
         </div>
 
-        {/* Eighth section: Image left, Desc right */}
-        <div className="flex flex-row gap-80 justify-center items-start mt-16">
-          <div className="flex-1 grid grid-cols-2 gap-6 w-fit">
+        {/* Section 8: Image left, Desc right */}
+        <div className="flex flex-row justify-center gap-50 mt-16">
+          <div className="grid grid-cols-2 gap-6 w-fit">
             <img src="/images/home_hero.png" alt="Image 1" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 2" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 3" className="w-60 h-48 object-cover rounded-lg" />
             <img src="/images/home_hero.png" alt="Image 4" className="w-60 h-48 object-cover rounded-lg" />
           </div>
           <div className="flex-[2] max-w-md flex flex-col items-center">
-            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold text-center">2018</h1>
+            <h1 className="text-[32px] bg-[#001F54] text-[#F4C261] px-4 py-2 rounded-xl w-fit mb-2 font-bold">2018</h1>
             <h1 className="font-bold text-[32px] text-center mb-2">Judul</h1>
             <p className="text-[26px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
-                    et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
-                    cursus, lobortis metus vitae, cursus arcu. 
-                </p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices justo ligula,
+              et rhoncus sapien luctus a. Sed scelerisque consectetur nisl ut tristique. Donec eu libero 
+              cursus, lobortis metus vitae, cursus arcu. 
+            </p>
           </div>
         </div>
 
