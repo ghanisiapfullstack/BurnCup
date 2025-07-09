4ae1b4670e3c50cfabb057ec1170509142f9917c
NotchG <vidoghibran7@gmail.com>
2025-07-10 01:42:11 +0700

    Update route.ts

--- burncup-fe/src/app/api/token/route.ts
+++ burncup-fe/src/app/api/token/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const secret = process.env.AUTH_SECRET ?? "x+moMx7qVg46NDBVCsxguFtF+Oja6uy7NdFNOlV+Z7M=";
-  const token = await getToken({ req, secret, raw: true });
+  const token = await getToken({ req, secret, raw: true, cookieName: "__Secure-next-auth.session-token" });
 
   if (!token) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
