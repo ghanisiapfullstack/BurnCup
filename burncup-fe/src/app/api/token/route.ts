2a3947129678fe54a45bf261fad09677abc5d175
NotchG <vidoghibran7@gmail.com>
2025-07-10 00:46:27 +0700

    Update route.ts

--- burncup-fe/src/app/api/token/route.ts
+++ burncup-fe/src/app/api/token/route.ts
@@ -1,9 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-const secret = process.env.AUTH_SECRET ?? "x+moMx7qVg46NDBVCsxguFtF+Oja6uy7NdFNOlV+Z7M=";
-
 export async function GET(req: Request) {
+  const secret = process.env.AUTH_SECRET ?? "x+moMx7qVg46NDBVCsxguFtF+Oja6uy7NdFNOlV+Z7M=";
   const token = await getToken({ req, secret, raw: true });
 
   if (!token) {
